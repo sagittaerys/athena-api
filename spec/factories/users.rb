61bca82ad394e0cc9fb4253ccc25b4f44805baae
Olamilekan Aremu <aremu0235@gmail.com>
2026-05-19 10:17:17 +0100

    feat(tests): wrote user model rspec test suite

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    username { "MyString" }
-    password_digest { "MyString" }
+    email { Faker::Internet.unique.email }
+    username { "#{Faker::Internet.username(specifier: 3..20).gsub(/[^a-zA-Z0-9_]/, '_')}" }
+    password_digest { "password123" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do 
+    subject(:user) { build(:user)}
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_uniqueness_of(:username).case_insensitive}
+    it { should validate_length_of(:username).is_at_least(3).is_at_most(30) }
+    it { should validate_length_of(:password).is_at_least(8)}
+  end
+  
+  describe "authentication" do
+    let(:user) { create(:user, password: "password123") }
+
+    it "authenticates with correct password" do
+      expect(user.authenticate("password123")).to eq(user)
+    end
+
+    it "fails authentication with wrong password" do
+      expect(user.authenticate("wrongpassword")).to be(false)
+    end
+  end
+
+  describe "password security" do
+    it "never stores plain text password" do
+      user = create(:user, password: "password123")
+      expect(user.password_digest).not_to eq("password123")
+    end
+  end
 end

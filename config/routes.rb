c17698564d7eb59b9b9f60d5dde92fd7c1a72e4f
bobai <bobai@duck.com>
2026-06-13 12:09:00 +0100

    Pressure-test Athena API with Ota contract matrix

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,21 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :voice_profiles, only: [ :show, :create, :destroy ] do
-        collection do
-          get :current
+      resources :library_items, only: [ :index, :show, :create, :destroy ] do
+        member do
+          post :parse_epub
         end
+      end
+
+      resources :library_items, only: [ :index, :show, :create, :destroy ]
+
+      resources :voice_profiles, only: [ :show, :create, :destroy ]
+
+      resources :library_items, only: [ :index, :show, :create, :destroy ] do
+        member do
+          post :parse_epub
+        end
+        resources :audio_chunks, only: [ :create, :show ]
       end
 
       resources :library_items, only: [ :index, :show, :create, :destroy ] do
